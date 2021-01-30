1821cea3d7176d65acb96003db7ba60f1be99876
Andrew Yatsura <andrewyazura203@gmail.com>
2021-01-30 20:11:31 +0200

    Only accept requests from localhost

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,11 +3,16 @@ import findUserByTelegram from '../services/users/findUserByTelegram';
 
 export default async (req, res, next) => {
   let headers = req.headers;
+  console.log(headers['host'])
 
-  if (headers['telegram_id']) {
-    req.user = await findUserByTelegram(headers['telegram_id']);
-  } else if (headers['id']) {
-    req.user = await findUser(headers['id']);
+  let regexp = /localhost:\d+/;
+
+  if (regexp.test(headers['host'])) {
+    if (headers['telegram_id']) {
+      req.user = await findUserByTelegram(headers['telegram_id']);
+    } else if (headers['id']) {
+      req.user = await findUser(headers['id']);
+    }
   }
 
   next();
