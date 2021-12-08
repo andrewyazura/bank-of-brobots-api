96b05ad4d16841252920ba3e3f79d7cf25e695bb
Andrew Yatsura <andrewyazura203@gmail.com>
2021-12-08 21:44:23 +0200

    Add custom exceptions and error handlers

---
+++ brobank_api/errors/handlers.py
@@ -0,0 +1,40 @@
+import re
+
+from brobank_api import db
+from brobank_api.errors import errors_bp
+from brobank_api.exceptions import CustomException
+from marshmallow import ValidationError
+from psycopg2.errors import UniqueViolation
+from sqlalchemy.exc import IntegrityError
+
+
+@errors_bp.app_errorhandler(404)
+def not_found_error(error):
+    return {"error": "Resource not found."}, 404
+
+
+@errors_bp.app_errorhandler(500)
+def internal_error(error):
+    db.session.rollback()
+    return {"error": "Internal error."}, 500
+
+
+@errors_bp.app_errorhandler(CustomException)
+def custom_exception(error):
+    return {"error": str(error)}, error.code
+
+
+@errors_bp.app_errorhandler(ValidationError)
+def marshmallow_error(error):
+    return {"error": "Validation error.", "errors": error.messages}, 400
+
+
+@errors_bp.app_errorhandler(IntegrityError)
+def invalid_token(error):
+    db.session.rollback()
+
+    if isinstance(error.orig, UniqueViolation):
+        field, value = re.findall(r"Key \((\w+)\)=\((\w+)\)", error.orig.pgerror)[0]
+        return {"error": f"{field=} with {value=} is already taken"}, 400
+
+    return {"error": "DB error."}, 400
---
+++ brobank_api/exceptions.py
@@ -0,0 +1,26 @@
+class CustomException(Exception):
+    code = 500
+
+
+class ExternalApplicationForbiddenIP(CustomException):
+    """Raise when request IP is not in external application's IP whitelist"""
+
+    code = 403
+
+
+class ExternalApplicationRestricted(CustomException):
+    """Raise when request is from restricted external application"""
+
+    code = 403
+
+
+class InvalidExternalApplicationToken(CustomException):
+    """Raise if external application token is invalid"""
+
+    code = 400
+
+
+class InvalidTelegramCallbackHash(CustomException):
+    """Raise when Telegram callback contains invalid hash"""
+
+    code = 400
