1a7e0e38ce2f0923b51a5951354c332f8e7622ac
Andrew Yatsura <andrewyazura203@gmail.com>
2021-12-09 21:43:56 +0200

    Bump python version and improve config.py

--- config.py
+++ config.py
@@ -6,9 +6,13 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, ".env"))
 
 
+def get_env_group(prefix):
+    return {k[len(prefix) :]: v for k, v in os.environ.items() if k.startswith(prefix)}
+
+
 class Config(object):
     SECRET_KEY = os.environ.get("SECRET_KEY")
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    TELEGRAM_BOT_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
+    TELEGRAM_BOT = get_env_group("TELEGRAM_BOT_")
